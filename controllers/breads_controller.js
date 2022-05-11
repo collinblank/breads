3f84694daa6bca5fbe2e30c35b147de85aeb900a
Collin Blank <collin@Collins-Mac-mini.local>
2022-05-11 09:43:05 -0400

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -3,12 +3,24 @@ const breads = express.Router()
 const Bread = require('../models/bread.js')
 //INDEX
 breads.get('/', (req, res) => {
-    res.send(Bread)
+    res.render('Index',
+    {
+        breads: Bread,
+        title: 'Index Page'
+    }
+)
+    //res.send(Bread)
 })
 
 //SHOW
-breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
+breads.get(`/:arrayIndex`, (req, res) => {
+ if (Bread[req.params.arrayIndex]){
+    res.render('Show', {
+        bread: Bread[req.params.arrayIndex]
+    })
+    }else {
+        res.send('404')
+    }
 })
 
 module.exports = breads
